cdd87a6de5f62375c8f60400b38f15474d449e92
Sanjeev <sanjeev.ksv@gmail.com>
2025-09-22 16:17:26 +0530

    userendheader.tsx

--- src/components/admin/userEnd-web/userEndHome/UserEndCommonButtons.tsx
+++ src/components/admin/userEnd-web/userEndHome/UserEndCommonButtons.tsx
@@ -1,168 +1,209 @@
-import { Box,Button, TextField, Typography,} from "@mui/material"
-import useUserEndwebStyles from "../UserendwebStyles"
-import AddIcon from '@mui/icons-material/Add';
+import { Box, Button, TextField, Typography } from "@mui/material";
+import useUserEndwebStyles from "../UserendwebStyles";
+import AddIcon from "@mui/icons-material/Add";
 import { useState } from "react";
 import UserendDeletepopup from "../../utils/UserendDeletepop";
-import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
+import FileUploadOutlinedIcon from "@mui/icons-material/FileUploadOutlined";
 
-
-type UserEndSaveCancelButtons<T =any>={
-  values:T,
-  onSave:(values: T)=>void;
-  
+interface SaveCancelProps {
+  onSave: () => void;
+  onCancel?: () => void;
+  disabled?: boolean;
 }
 
-export const UserEndSaveCancelButtons = () => {
+export const UserEndSaveCancelButtons = ({
+  onSave,
+  onCancel,
+  disabled = false,
+}: SaveCancelProps) => {
+  const { classes } = useUserEndwebStyles();
 
-    const{classes}=useUserEndwebStyles()  
-    
-    const handleSave=()=>{
-      
-    }
-
-    return (
-   
-    <Box className={classes.buttonContainer}>
-         <Button className={classes.headerSaveButton}
-          onClick={handleSave}>Save</Button>
-         <Button className={classes.headerCancelButton}>Cancel</Button>  
-    </Box>
-
-    
-  )
-}
-
-export const UserendSaveDeleteButtons=({message}:{message:string})=>{
-
-const[open,setOpen]=useState(false)
-
-const handleToggle=()=>{
-  setOpen((prev)=>!prev)
-}
-
-const handleConfirmDelete=()=>{
-   console.log("Deleted",message);
-   setOpen(false)
-}
-
-const{classes}=useUserEndwebStyles() 
-
-return(
-    <Box display="flex" gap={2}>
-     <Button  className={classes.heroSave} >Save</Button>
-     <Button  className={classes.heroDelete} onClick={handleToggle}>Delete</Button>
-    {open&& <UserendDeletepopup 
-    open={open} 
-    message={message}
-    onClose={handleToggle}
-    onDelete={handleConfirmDelete}/>} 
-   </Box>
-  )
-
-}
-
-export const AddingButton=()=>{
-
-  const{classes}=useUserEndwebStyles() 
-    
-    return(
-       <Box className={classes.addingButtonBox}>
-    <Button variant="contained" startIcon={<AddIcon />} className={classes.AddingButton} >
-     Add Slide
-     </Button>
-   </Box> 
-    ) 
-}
-
-export const DeleteButton=({message}:{message:string})=>{
-
-const{classes}=useUserEndwebStyles() 
-
-const[open,setOpen]=useState(false)
-
-const handleToggle=()=>{
-  setOpen((prev)=>!prev)
-}
-const handleConfirmDelete=()=>{
-  console.log("Delete",message)
-}
-
-return (
-    <Box>
-     <Button variant="outlined" className={classes.heroDelete}
-     onClick={handleToggle}>Delete</Button>
-    <UserendDeletepopup 
-    open={open}
-    message={message}
-    onClose={handleToggle}
-    onDelete={handleConfirmDelete}/>
-    </Box>
-  )
-}
-
-export const Textfiledbox=()=>{
-  
-  const{classes}=useUserEndwebStyles() 
- 
-  return(
-  
-    <TextField className={classes.heroTextfiled}
-            fullWidth
-            multiline
-            minRows={5}/>
-  )
-}
-
-export const Uploadbutton=()=>{
-  const{classes}=useUserEndwebStyles() 
-  
-  return(
-    <Button variant="outlined" className={classes.uploadHerobutton}
-          component="label" endIcon={<FileUploadOutlinedIcon />}>
-          <input type="file"
-          accept="image/*"
-          hidden
-          /> Upload</Button>
-   )
-}
-
-export const TextFieldManyRows=()=>{
-  const{classes}=useUserEndwebStyles() 
-  return(
-    <TextField className={classes.heroTextfiled}
-          fullWidth
-         multiline
-         minRows={5}/>
-  )
-}
-
-export const ErrorMessages=()=>{
-  const{classes}=useUserEndwebStyles() 
   return (
-   <Typography className={classes.errorUpload}>
-                 *Please upload the image in landscape format (Preferred size: 300px width × 100px height)
-               <Typography className={classes.errorUpload}>Image Must be 5 MB</Typography>
-              </Typography>
-  )
+    <Box className={classes.buttonContainer}>
+      <Button
+        className={classes.headerSaveButton}
+        onClick={onSave}
+        disabled={disabled}
+        variant="contained"
+      >
+        Save
+      </Button>
+      <Button className={classes.headerCancelButton} onClick={onCancel}>
+        Cancel
+      </Button>
+    </Box>
+  );
+};
+
+interface GenericSavedelete {
+  message: string;
+  onDelete: () => void;
 }
 
-export const ErrormsgTitle=()=>{
-    const{classes}=useUserEndwebStyles()  
-  return(
-    <Typography className={classes.errorUpload}>Max 200 characters required</Typography> 
-   )
+export const UserendSaveDeleteButtons = ({
+  message,
+  onDelete,
+}: GenericSavedelete) => {
+  const [open, setOpen] = useState(false);
+
+  const handleToggle = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const handleConfirmDelete = () => {
+    console.log("Deleted", message);
+    onDelete();
+    setOpen(false);
+  };
+
+  const { classes } = useUserEndwebStyles();
+
+  return (
+    <Box display="flex" gap={2}>
+      <Button className={classes.heroSave}>Save</Button>
+      <Button className={classes.heroDelete} onClick={handleToggle}>
+        Delete
+      </Button>
+      {open && (
+        <UserendDeletepopup
+          open={open}
+          message={message}
+          onClose={handleToggle}
+          onDelete={handleConfirmDelete}
+        />
+      )}
+    </Box>
+  );
+};
+
+interface Genericadding {
+  onClick?: () => void;
 }
 
-export const ErrormsgContent=()=>{
-    const{classes}=useUserEndwebStyles()  
-  return(
-    <Typography className={classes.errorUpload}>Max 2000 characters required</Typography> 
-   )
-}
+export const AddingButton = ({ onClick }: Genericadding) => {
+  const { classes } = useUserEndwebStyles();
 
-export const ErrormsgPrice=()=>{
-    const{classes}=useUserEndwebStyles()  
-  return(
-    <Typography className={classes.errorUpload}>Max 12 characters required</Typography> 
-   )
-}
+  return (
+    <Box className={classes.addingButtonBox}>
+      <Button
+        variant="contained"
+        startIcon={<AddIcon />}
+        className={classes.AddingButton}
+        onClick={onClick}
+      >
+        Add Slide
+      </Button>
+    </Box>
+  );
+};
+
+export const DeleteButton = ({ message }: { message: string }) => {
+  const { classes } = useUserEndwebStyles();
+
+  const [open, setOpen] = useState(false);
+
+  const handleToggle = () => {
+    setOpen((prev) => !prev);
+  };
+  const handleConfirmDelete = () => {
+    console.log("Delete", message);
+  };
+
+  return (
+    <Box>
+      <Button
+        variant="outlined"
+        className={classes.heroDelete}
+        onClick={handleToggle}
+      >
+        Delete
+      </Button>
+      <UserendDeletepopup
+        open={open}
+        message={message}
+        onClose={handleToggle}
+        onDelete={handleConfirmDelete}
+      />
+    </Box>
+  );
+};
+
+export const Textfiledbox = () => {
+  const { classes } = useUserEndwebStyles();
+
+  return (
+    <TextField
+      className={classes.heroTextfiled}
+      fullWidth
+      multiline
+      minRows={5}
+    />
+  );
+};
+
+export const Uploadbutton = () => {
+  const { classes } = useUserEndwebStyles();
+
+  return (
+    <Button
+      variant="outlined"
+      className={classes.uploadHerobutton}
+      component="label"
+      endIcon={<FileUploadOutlinedIcon />}
+    >
+      <input type="file" accept="image/*" hidden /> Upload
+    </Button>
+  );
+};
+
+export const TextFieldManyRows = () => {
+  const { classes } = useUserEndwebStyles();
+  return (
+    <TextField
+      className={classes.heroTextfiled}
+      fullWidth
+      multiline
+      minRows={5}
+    />
+  );
+};
+
+export const ErrorMessages = () => {
+  const { classes } = useUserEndwebStyles();
+  return (
+    <Typography className={classes.errorUpload}>
+      *Please upload the image in landscape format (Preferred size: 300px width
+      × 100px height)
+      <Typography className={classes.errorUpload}>
+        Image Must be 5 MB
+      </Typography>
+    </Typography>
+  );
+};
+
+export const ErrormsgTitle = () => {
+  const { classes } = useUserEndwebStyles();
+  return (
+    <Typography className={classes.errorUpload}>
+      Max 200 characters required
+    </Typography>
+  );
+};
+
+export const ErrormsgContent = () => {
+  const { classes } = useUserEndwebStyles();
+  return (
+    <Typography className={classes.errorUpload}>
+      Max 2000 characters required
+    </Typography>
+  );
+};
+
+export const ErrormsgPrice = () => {
+  const { classes } = useUserEndwebStyles();
+  return (
+    <Typography className={classes.errorUpload}>
+      Max 12 characters required
+    </Typography>
+  );
+};
--- src/components/admin/userEnd-web/userEndHome/UserendHero.tsx
+++ src/components/admin/userEnd-web/userEndHome/UserendHero.tsx
@@ -4,29 +4,46 @@ import fishImg from './../../../../assets/admin/fishImg.jpg'
 import CancelIcon from '@mui/icons-material/Cancel';
 import { UserEndSaveCancelButtons, UserendSaveDeleteButtons,AddingButton, Uploadbutton, TextFieldManyRows, ErrorMessages, ErrormsgTitle } from "./UserEndCommonButtons";
 import { Fragment } from "react/jsx-runtime";
+import { useState } from "react";
+import {v4 as uuidv4} from "uuid";
 
 const UserendHero = () => {
 
 const{classes}=useUserEndwebStyles() 
-const heroslides=[
-  {id:1,name:"Slide1"},
-  {id:2,name:"Slide2"},
-]
+const[heroslide,setHeroslide]=useState([
+     {id:uuidv4(),name:"Slide1"},
+     {id:uuidv4(),name:"Slide2"},
+ ])
 
+
+const handleAddSlide=()=>{
+  const newSlide= {id:uuidv4(),name: `Slide${heroslide.length+1}`}
+  const updateSlides=[...heroslide,newSlide]
+  setHeroslide(updateSlides)
+  console.log("slide adding after",updateSlides )
+}
+
+const onDelete=(id:string)=>{
+  const updatedslides=heroslide.filter((slide)=>
+  slide.id!==id);
+  setHeroslide(updatedslides)
+  console.log("Slide after deletion",updatedslides)
+}
 
 return (
    <Box>    
    <Box className={classes.useHerocontainer}>
-   <AddingButton />
+   <AddingButton onClick={handleAddSlide}/>
    
    {
-    heroslides.map((slide,index)=>(
+    heroslide.map((slide,index)=>(
 
       <Fragment>
-            <Box mt={2}>
-   <Stack className={classes.slideAndButtons}>
+      <Box mt={2}>
+     <Stack className={classes.slideAndButtons}>
     <Typography className={classes.titleText}>{slide.name}</Typography>
-    <UserendSaveDeleteButtons message={`Are you sure want to delete ${slide.name} ?`}/>
+    <UserendSaveDeleteButtons message={`Are you sure want to delete ${slide.name} ?`}
+     onDelete={()=>onDelete(slide.id)}/>
    </Stack>
    <Stack className={classes.Uploadandheadingbox}>
      <Stack className={classes.UploadImageStack}>
@@ -44,10 +61,9 @@ return (
      <ErrormsgTitle />   
      </Stack>
    </Stack>
-   {index!==heroslides.length-1&&<Divider className={classes.heroDivider}/>}
- 
-   </Box>
-      </Fragment>
+   {index!==heroslide.length-1&&<Divider className={classes.heroDivider}/>}
+  </Box>
+  </Fragment>
     ))
    }
 
