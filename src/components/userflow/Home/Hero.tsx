34c66a0accf138a545e8213503559724229d97b6
venkata-hari <xprogrammer.me@gmail.com>
2025-10-15 15:04:29 +0530

    version2 is implmented

--- src/components/userflow/Home/Hero.tsx
+++ src/components/userflow/Home/Hero.tsx
@@ -1,13 +1,13 @@
 // src/components/userflow/Home/Hero.tsx
 import { Box, Typography, useMediaQuery } from "@mui/material";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 // import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 // import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { useTheme } from "@mui/material/styles";
 import heroImg from "../../../assets/home/hero_img.png";
 import useHomeStyles from "./homeStyles";
 // import sunfish from '../../../assets/home/sunfish.gif'
-import giphyunscreen from '../../../assets/home/giphy-unscreen.gif'
+// import giphyunscreen from '../../../assets/home/giphy-unscreen.gif'
 
 import {
   CULTURES_FISH_INITIAL,
@@ -15,6 +15,10 @@ import {
   CULTURES_FISH_TRANSITION,
 } from "../Shared/animations";
 import BubbleCanvas from "../../animations/BubbleCanvas";
+import KoiFishScene from "../../animations/KoiFishScene";
+import Fish3DModel from "../../animations/Fish3DModel";
+import SwimmingFish from "../../animations/SwimmingFish";
+import { motion } from "framer-motion";
 
 
 const Hero = () => {
@@ -25,39 +29,37 @@ const Hero = () => {
   return (
     <Box className={classes.heroRoot} sx={{position:'relative'}}>
     <Box className="herofish">
-      <Box
-      component="img"
-      src={giphyunscreen}
-      className='fish1'
-     alt='img'
-    />
-    {/* <FishAnimation/>
-    <BlueFishAnimation/> */}
+    <SwimmingFish  Position="absolute" Count={10} Height={3000}/>
+
+
    {/* <Box
       component="img"
       src={angerfish}
       className='fish1'
      alt='img'
     /> */}
-      <Box
+      {/* <Box
       component="img"
       src={giphyunscreen}
       className='fish2'
      alt='img'
-    />
+    /> */}
+    
+
        </Box>
       <Box className={classes.heroMain}>
       
         <Box className={classes.heroImgBox}>
        
-          <motion.img
+          {/* <motion.img
             src={heroImg}
             alt="Hero"
             className={classes.heroImg}
             initial={CULTURES_FISH_INITIAL}
             animate={CULTURES_FISH_ANIMATE}
             transition={CULTURES_FISH_TRANSITION}
-          />
+          /> */}
+          <Fish3DModel/>
         </Box>
         <Box className={classes.heroTextBoxWrap} >
           <Box className={classes.heroTextBox}>
