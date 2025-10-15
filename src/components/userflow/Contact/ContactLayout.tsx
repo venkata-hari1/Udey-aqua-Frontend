ee23886bbb359861b04f45b4c2d63a5a3c4bfaf9
Vinodhiniyeleti <vinodhiniyeleti1@gmail.com>
2025-10-15 18:51:44 +0530

    contact hero image is changed

--- src/components/userflow/Contact/ContactLayout.tsx
+++ src/components/userflow/Contact/ContactLayout.tsx
@@ -1,6 +1,9 @@
 // src/components/userflow/Contact/ContactLayout.tsx
 import { Box } from "@mui/material";
 import useContactStyles from "./contactStyles";
+import Hero from "../common/Hero/Hero";
+import SwimmingFish from  "../../../components/animations/SwimmingFish";
+import { Grid } from "@mui/material";
 
 interface ContactLayoutProps {
   children: React.ReactNode;
@@ -11,6 +14,14 @@ const ContactLayout = ({ children }: ContactLayoutProps) => {
 
   return (
     <div className={classes.contactLayoutRoot}>
+      <SwimmingFish  Position="absolute" Count={30} Height={3000}/>
+            <Grid  size={{ xs: 12 }}>
+              <Hero
+                page="contact" 
+                overlayColor="rgba(10,79,164,0.41)" 
+                fishHeight={500}
+              />
+            </Grid>
       <Box className={classes.contactMainRow}>
         <Box className={classes.contactContent}>{children}</Box>
       </Box>
